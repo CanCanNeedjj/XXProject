06fc8b8aac088f3fcd3b205130948bf8922f5a59
Qian Shang <sp2264522742@outlook.com>
2023-12-06 17:03:06 +0800

    后端：修改JDK版本为8，SpringBoot版本为2.7.10

--- serve/src/main/java/com/edu/controller/ChatController.java
+++ serve/src/main/java/com/edu/controller/ChatController.java
@@ -3,10 +3,12 @@ package com.edu.controller;
 
 import com.edu.chatutil.Chat;
 import com.edu.util.Result;
-import jakarta.annotation.Resource;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class ChatController {
